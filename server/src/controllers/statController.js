bfe9f4a99b6c3a6f2d4ca8a2afb99bf0e256f010
terriadsit <terriadsit@yahoo.com>
2022-11-28 06:58:01 -0600

    statController updating mongoDb doc correctly

--- server/src/controllers/statController.js
+++ server/src/controllers/statController.js
@@ -1,31 +1,39 @@
+const mongoose = require('mongoose')
+const Statistic = require('../models/statModel')
+
 const getStats = (req, res) => {
-    console.log('in getStats path')
-    return res.status(200).json({
-        message: 'in getStats path'
-    })
+  console.log('in getStats path')
+  return res.status(200).json({
+    message: 'in getStats path'
+  })
 }
 
 const updateStats = async (req, res) => {
-       const { id } = req.params
-        
-        console.log('req.body', req.body, 'id', id)
+  const { id } = req.params
 
-       // const field = req.body.field     
-        
-        // const statistics = await User.findOneAndUpdate({_id: id}, { $inc: {[field]: 1} }, {
-        //     new: true,
-        //     upsert: true,
-        // })
-    
-        // if (!statistics) {
-        //     return res.status(404).json({error: 'Error updating statistics'})
-        // }
-    
-        res.status(200).json({message: 'in updateStats'})
-    
+  console.log('req.body', req.body, 'id', id)
+  // set key value object to be increased
+  const key = req.body.change
+  let obj = {}
+  obj[key] = 1
+  console.log('obj', obj)
+  const statistics = await Statistic.findOneAndUpdate(
+    { googleId: id },
+    { $inc: obj },
+    {
+      new: true,
+      upsert: true
+    }
+  )
+
+  if (!statistics) {
+    return res.status(404).json({ error: 'Error updating statistics' })
+  }
+
+  res.status(200).json(statistics)
 }
 
 module.exports = {
-    getStats,
-    updateStats
+  getStats,
+  updateStats
 }
