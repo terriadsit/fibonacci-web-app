b0f193f15c8e80c91cdb4ef548e4f6734ac95f28
terriadsit <terriadsit@yahoo.com>
2022-12-07 07:01:18 -0600

    change from https to http for CI/CD

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
 const path = require('path');
 const { mongoConnect } = require('./services/mongo');
-const http = require("https");  // change https to http and sockets work in online page
+const http = require("http");  // change https to https and uncomment key, cert for https
 const io = require('socket.io'); // also change ENDPOINT in client/online.js to http
 
 const apiServer = require("./api");  // the express handler
 
 const options = {
-  key: fs.readFileSync(path.join(__dirname,'key.pem')), // comment these lines out and sockets work in online
-  cert: fs.readFileSync(path.join(__dirname,'cert.pem')), // comment these lines out and sockets work in online
+//  key: fs.readFileSync(path.join(__dirname,'key.pem')), // comment these lines out and sockets work in online
+//  cert: fs.readFileSync(path.join(__dirname,'cert.pem')), // comment these lines out and sockets work in online
 }
 
 const httpServer = http.createServer(options, apiServer);
@@ -25,18 +25,7 @@ const socketServer = io(httpServer, {
 const sockets = require('./sockets')
 sockets.listen(socketServer);
 
-// connect to db
-//await mongoConnect();
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => {
-    // listen for requests
-   // httpServer.listen(port);
-   // console.log(`listening on port ${port}`)      
-  // })
-  // .catch((error) => {
-  //   console.log(error)
-  // })
-  async function startServer() {
+async function startServer() {
     await mongoConnect();
    
     httpServer.listen(port, () => {
